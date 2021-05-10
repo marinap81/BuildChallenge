99250796afe69cb3f7ba551fef899ea33c13d1b3
Marina Pajvancic <103340660@student.swin.edu.au>
2021-05-10 15:23:22 +1000

    Task 2 Completed

--- BuildChallenge.sql
+++ BuildChallenge.sql
@@ -1,23 +1,40 @@
 CREATE DATABASE BUILDCHALLENGE;
+USE BUILDCHALLENGE;
 
 --MARINA PAJVANCIC
 --STUDENT NO: 103340660
---RELATIONAL SCHEMA
-
---TOUR (TourName, Description)
--- PK (TourName)
-
---EVENT(EventYear, EventMonth,EventDay,EventFee)
---PK (EventYear, EventMonth, EventDay)
-
---CLIENT(ClientID, Surname, GivenName,Gender)
---PK (ClientID)
-
---BOOKING (DateBooked, Payment, ClientID, EventYear,EventMonth,EventDay)
---FK (ClientID) references CLIENT table
---FK(EventYear, EventMonth, EventDay) references EVENT table
 
 
+CREATE TABLE CLIENT
+(ClientID INT PRIMARY KEY,
+SURNAME NVARCHAR(100) NOT NULL,
+GivenName NVARCHAR(100) NOT NULL,
+Gender NVARCHAR(1));
+
+CREATE TABLE TOUR
+(TourName NVARCHAR(100) PRIMARY KEY,
+Description NVARCHAR(500));
+
+CREATE TABLE EVENT
+(EventYear INT,
+EventMonth NVARCHAR(3),
+EventDay INT,
+TourName NVARCHAR(100),
+EventFee MONEY,
+CONSTRAINT PK_EvenT PRIMARY KEY (EventYear, EventDay, EventMonth, TourName),
+CONSTRAINT FK_Event_TourName FOREIGN KEY (TourName) REFERENCES TOUR (TourName));
+
+
+CREATE TABLE BOOKING
+(ClientID INT NOT NULL,
+TourName NVARCHAR(100),
+EventMonth NVARCHAR(3),
+EventDay INT,
+EventYear INT,
+Payment MONEY,
+DateBooked DATE,
+CONSTRAINT FK_Booking_ClientID FOREIGN KEY (ClientID) REFERENCES CLIENT (ClientID),
+CONSTRAINT FK_Booking_Event FOREIGN KEY (EventYear, EventDay, EventMonth, TourName) REFERENCES EVENT);
 
 
 
